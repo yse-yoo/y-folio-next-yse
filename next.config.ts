553f136187dd6541b95b0cfa2141ff84385a0c13
yse-yoo <yse-yoo@yse-c.net>
2025-12-01 16:30:25 +0900

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ESLintのビルド時チェックを無効化
+    ignoreDuringBuilds: true,
+  },
+  
 };
 
 export default nextConfig;
